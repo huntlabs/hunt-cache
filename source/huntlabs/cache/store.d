227a8e8e4d700b9d80697bd64ce5ba81e437d1c9
zhangyuchun <encounterspring@163.com>
2018-03-28 17:09:20 +0800

    add

---
+++ source/huntlabs/cache/store.d
@@ -0,0 +1,33 @@
+﻿module huntlabs.cache.store;
+
+import kiss.serialize;
+import huntlabs.cache.nullable;
+//only add header from Serialize for check meta info.
+//	meta
+//	object
+
+import std.stdio;
+
+byte[] SerializeToByte(T)( T t)
+{
+	import std.stdio;
+	string meta = immutable(T).stringof;
+
+	return serialize(meta[10 .. $-1]) ~ serialize(t);
+}
+
+ Nullable!T DeserializeToObject(T)(const byte[] data )
+{
+	long parser_index;
+	if(data.length == 0)
+		return Nullable!T.init;
+
+	string meta = unserialize!string(data , parser_index);
+	if(immutable(T).stringof[10 .. $-1] != meta)
+		return Nullable!T.init;
+	Nullable!T nullt;
+
+	nullt.bind(unserialize!T(data[cast(size_t)parser_index .. $]));
+
+	return nullt;
+}
