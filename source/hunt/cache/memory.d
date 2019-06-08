1b6ea3ab5cf7c706e1b70141441a0a296746e646
shove <shove@163.com>
2019-06-08 13:13:20 +0800

    Temporarily cancel the use of Radix.
    Temporarily cancel the use of Radix and replace it with self-built associative arrays in D language for storage
    
    Please refer to:
    https://github.com/huntlabs/hunt/issues/56
    
    If you decide to fix Radix, you can ignore this PR.

--- source/hunt/cache/memory.d
+++ source/hunt/cache/memory.d
@@ -1,218 +1,197 @@
-﻿module hunt.cache.memory;
+module hunt.cache.memory;
 
 import hunt.cache.cache;
 import hunt.cache.store;
 import hunt.cache.nullable;
 
-import hunt.collection.Radix;
-
 import core.stdc.stdlib;
 import core.stdc.string;
 import core.stdc.time;
 
-import std.stdio;
+import std.variant;
+import std.conv;
 
 public:
-class MemoryCache 
+
+class MemoryCache
 {
+    Nullable!V get(V)(string key)
+    {
+        synchronized(this)
+        {
+            return get_inter!V(key);
+        }
+    }
 
-//interface
+    Nullable!V[string] getall(V)(string[] keys)
+    {
+        Nullable!V[string] mapv;
 
-	Nullable!V get(V)(string key)
-	{
-		synchronized(this)
-		{
-			return get_inter!V(key);
-		}
-	}
+        synchronized(this)
+        {
+            foreach(k; keys)
+            {
+                mapv[k] = get_inter!V(k);
+            }
+        }
 
-	Nullable!V[string] getall(V)(string[] keys)
-	{
-		Nullable!V[string] mapv;
-		synchronized(this)
-		{
-			foreach(k ; keys)
-			mapv[k] = get_inter!V(k);
-		}
+        return mapv;
+    }
 
-		return mapv;
-	}
+    bool containsKey(string key)
+    {
+        synchronized(this)
+        {
+            return (key in cacheData) ? true : false;
+        }
+    }
 
-	bool containsKey(string key)
-	{
-		synchronized(this){
-			return find_inter(key , true);
-		}
-	}
-	
-	void put(V)(string key ,  V v , uint expired = 0)
-	{
-		synchronized(this)
-		{
-			put_inter(key , v , expired);
-		}
-	}
+    void put(V)(string key, V v, uint expired = 0)
+    {
+        synchronized(this)
+        {
+            put_inter(key, v, expired);
+        }
+    }
 
-	bool putifAbsent(V)(string key ,  V v)
-	{
-		synchronized(this)
-		{
-			if(!find_inter(key , false))
-			{	
-				put_inter(key , v , 0);
-				return true;
-			}
-			return false;	
-		}		
-	}
+    bool putifAbsent(V)(string key, V v)
+    {
+        synchronized(this)
+        {
+            bool ret = (key in cacheData) ? true : false;
 
-	void putAll(V)( V[string] maps , uint expired = 0)
-	{
-		synchronized(this){
-			foreach(k , v ; maps)
-			{
-				put_inter(k , v , expired);
-			}
-		}
-	}
-	
-	bool remove(string key)
-	{
-		synchronized(this)
-		{
-			return remove_inter(key);
-		}
-	}
+            if (!ret)
+            {
+                put_inter!V(key, v, 0);
 
-	void removeAll(string[] keys)
-	{
-		synchronized(this)
-		{
-			foreach( k ; keys)
-			{
-				remove_inter(k);
-			}
-		}
-	}
+                return true;
+            }
 
-	void clear()
-	{
-		synchronized(this){
-			rax_data.Clear();
-			rax_time.Clear();
-		}
-	}
+            return false;
+        }
+    }
 
-	this(string args = "")
-	{
-		rax_data = rax.New();
-		rax_time = rax.New();
-	}
+    void putAll(V)(V[string] maps, uint expired = 0)
+    {
+        synchronized(this)
+        {
+            foreach(k, v; maps)
+            {
+                put_inter(k, v, expired);
+            }
+        }
+    }
 
-	~this()
-	{
-		rax.Free(rax_data);
-		rax.Free(rax_time);
-	}
+    bool remove(string key)
+    {
+        synchronized(this)
+        {
+            if (key in cacheTime)
+            {
+                cacheTime.remove(key);
+            }
+
+            bool ret = (key in cacheData) ? true : false;
+
+            if (ret)
+            {
+                cacheData.remove(key);
+            }
+
+            return ret;
+        }
+    }
+
+    void removeAll(string[] keys)
+    {
+        synchronized(this)
+        {
+            foreach(k; keys)
+            {
+                if (k in cacheTime)
+                {
+                    cacheTime.remove(k);
+                }
+
+                if (k in cacheData)
+                {
+                    cacheData.remove(k);
+                }
+            }
+        }
+    }
+
+    void clear()
+    {
+        synchronized(this)
+        {
+            cacheData.clear();
+            cacheTime.clear();
+        }
+    }
+
+    this(string args = "")
+    {
+    }
+
+    ~this()
+    {
+        clear();
+    }
 
 protected:
 
-	rax *rax_data;
-	rax *rax_time;
+    Variant[string] cacheData;
+    uint[string] cacheTime;
 
-	bool find_inter(string key , bool free)
-	{
-		Nullable!uint tick = get_inter!uint(rax_time , key);
-		if(tick.isnull)
-		{
-			void *data;
-			return rax_data.find(cast(ubyte[])key ,data);
-		}
-		else{
-			uint now = cast(uint)time(null);
-			if( tick.origin < now )
-			{
-				if(free)
-				{
-					remove_inter(key);
-				}
-				return false;
-			}
-			else{
-				return true;
-			}
-		}
-	}
+    Nullable!V get_inter(V)(string key)
+    {
+        Nullable!V v;
 
-	Nullable!V get_inter(V)(string key)
-	{
-		Nullable!uint tick =  get_inter!uint(rax_time , key);
+        if (key !in cacheTime)    //not set ttl
+        {
+            if (key in cacheData)
+            {
+                v.bind(cacheData[key].get!V);
+            }
 
-		if(tick.isnull)	//not set ttl
-		{
-			return get_inter!V(rax_data , key);
-		}
-		else{
-			uint now = cast(uint)time(null);
-			if( tick.origin < now) // remove
-			{
-				remove_inter(key);
-				return Nullable!V.init;
-			}
-			else
-			{
-				return get_inter!V(rax_data , key);
-			}
-		}
-	}
+            return v;
+        }
+        else
+        {
+            uint tick = cacheTime[key];
+            uint now = cast(uint)time(null);
 
-	Nullable!V get_inter(V)(rax *r ,string key)
-	{
-		void *data;
-		if (!r.find(cast(ubyte[])key , data))
-			return Nullable!V.init;
+            if (tick < now) // remove
+            {
+                cacheTime.remove(key);
+                cacheData.remove(key);
 
-		uint len;
-		memcpy(&len , data , 4);
-		byte[] byDatas = new byte[len];
-		memcpy(byDatas.ptr , data + 4, len);
-		return DeserializeToObject!V(byDatas);
-	}
+                return v;
+            }
+            else
+            {
+                v.bind(cacheData[key].get!V);
 
-	void put_inter(V)(string key , V v , uint expired)
-	{
-		if(expired == 0)
-			put_inter(rax_data , key , v);
-		else{
-			put_inter!uint(rax_time , key , expired + cast(uint)time(null));
-			put_inter(rax_data , key , v);
-		}
-	}
+                return v;
+            }
+        }
+    }
 
-	void put_inter(V)(rax *r , string key ,  V v)
-	{
-		byte[] data = SerializeToByte(v);
+    void put_inter(V)(string key, V v, uint expired)
+    {
+        if (expired == 0)
+        {
+            if (key in cacheTime)
+            {
+                cacheTime.remove(key);
+            }
+        }
+        else
+        {
+            cacheTime[key] = expired + cast(uint)time(null);
+        }
 
-		void *value = malloc(data.length + 4);
-		uint len = cast(uint) data.length;
-		memcpy(value , &len , 4);
-		memcpy(value + 4 , data.ptr , data.length);
-		r.Insert(cast(ubyte[])key , value);
-	}
-
-	bool remove_inter(string key)
-	{
-		if(rax_data.Remove(cast(ubyte[])key))
-		{
-			rax_time.Remove(cast(ubyte[])key);
-			return true;
-		}
-
-		return false;
-	}
-
-	bool remove_inter(rax *r , string key)
-	{	
-		return r.Remove(cast(ubyte[])key);	
-	}
+        cacheData[key] = v;
+    }
 }
