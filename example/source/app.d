722ade3d1e71b664fc917d03f670933da7c57d53
zoujiaqing <zoujiaqing@gmail.com>
2019-06-25 19:57:57 +0800

    Fix example

--- example/source/app.d
+++ example/source/app.d
@@ -1,113 +1,37 @@
 module example;
+
+import hunt.logging;
 import hunt.cache;
 
-struct Student
+struct User
 {
-    ulong         ID;
-    string         name;
-    string        address;
-}
-
-class Grade
-{
-    int         gradeLevel;
-    string      gradeName;
-    Student[]    arrStu;
+	string name;
+	int age;
 }
 
 import std.conv;
 
-void example(UCache cache)
+void example(Cache cache)
 {
-    import std.stdio;
-    //string
+	// define key
+	string key = "userinfo";
 
-    cache.put!string("test", "teststring");
-    string val = cache.get!string("test");
+	User user;
+	user.name = "zoujiaqing";
+	user.age = 99;
 
-    cache.put!string("test1","");
-    auto val2 = cache.get_ex!string("test1");
-    assert(!val2.isnull);
+	// set value
+	cache.set(key, user);
 
-    //student.
-    Student stu1;
-    stu1.ID = 1;
-    stu1.name = "tom";
-    stu1.address = "Tianlin Road 1016";
-    cache.put!Student("tom", stu1);
+	// get value
+	auto userinfo = cache.get!User(key);
 
-    Student stu = cache.get!Student("tom");
-
-    Nullable!Student nstu = cache.get_ex!Student("tom");
-    assert(!nstu.isnull);
-
-    Student stu2 = nstu.origin;
-    Student stu3 = cast(Student)nstu;
-    assert(stu2 == stu);
-    assert(stu2 == stu);
-    assert(stu3 == stu);
-
-    auto nstu2 = cache.get!Student("jacker");
-    assert(nstu2 == Student.init);
-
-    Nullable!Student nstu3 = cache.get_ex!Student("test");
-    assert(nstu3.isnull);
-
-    //Grade
-    Grade grade = new Grade();
-    grade.gradeLevel = 12;
-    grade.gradeName = "13";
-    grade.arrStu ~= stu1;
-
-    cache.put!Grade("13", grade);
-
-    auto gra = cache.get!Grade("tom");
-    assert(gra is null);
-
-    auto gra1 = cache.get!Grade("13");
-    assert(gra1 !is null);
-    writeln(gra1.gradeLevel, " ", gra1.gradeName, " ", gra1.arrStu);
-
+	logDebug(userinfo.name);
 }
 
-void example2()
+void main()
 {
-    CacheManger manger = new CacheManger();
-
-    manger.createCache("default");
-
-    manger.createCache("myredis", "redis", "127.0.0.1:6379");
-    manger.createCache("myreids_l2", "redis", "127.0.0.1:6379",true);
-
-    manger.createCache("mymemcached", "memcached", "--server=127.0.0.1:11211");
-
-    example(manger.getCache("default"));
-
-    example(manger.getCache("myredis"));
-
-    example(manger.getCache("myreids_l2"));
-
-    example(manger.getCache("mymemcached"));
-
-}
-
-/*
-void example3(UCache cache)
-{
-    const int i = 12;
-    cache.put!int("count", i);
-    cache.put("count_const", i);
-    assert(cast(int)cache.get!int("count_const") == i);
-}*/
-
-int main()
-{
-    auto cache = UCache.CreateUCache();
+    auto cache = CacheFectory.create();
 
     example(cache);
-
-    example2();
-
-    return 0;
-
 }
